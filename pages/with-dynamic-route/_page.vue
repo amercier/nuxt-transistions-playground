376352b51c324f5d22d90d2855200b91e8913916
Alex Mercier <pro.alexandre.mercier@gmail.com>
2021-02-23 14:34:56 +0100

    Add example with dynamic route

---
+++ pages/with-dynamic-route/_page.vue
@@ -0,0 +1,31 @@
+<template>
+  <Component :is="pageComponent" />
+</template>
+
+<script>
+import Step1Page from './-step1.vue'
+import Step2Page from './-step2.vue'
+import Step3Page from './-step3.vue'
+
+const PAGES = {
+  step1: Step1Page,
+  step2: Step2Page,
+  step3: Step3Page,
+}
+
+export default {
+  key: 'with-dynamic-route',
+
+  asyncData({ params }) {
+    return {
+      page: params.page,
+    }
+  },
+
+  computed: {
+    pageComponent() {
+      return PAGES[this.page]
+    },
+  },
+}
+</script>
